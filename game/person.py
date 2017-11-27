b547ca572066e4201b22cfe58527ea8821613580
newbazz <krutamhathi14@gmail.com>
2017-11-27 16:01:05 +0530

    coala applied

--- game/person.py
+++ game/person.py
@@ -1,13 +1,15 @@
 import os
 import sys
 
+
 class Person:
-	def __init__(self):
-		self.x = 0
-		self.y = 0
-		self.living = 1
-		self.typ = 1
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.living = 1
+        self.typ = 1
+
 
 '''
-	typ is 1 for bomberman and 0 for enemy
+    typ is 1 for bomberman and 0 for enemy
 '''
